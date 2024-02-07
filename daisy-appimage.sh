db31a3d72a6461c2f65c4df8c3a73da2fd4080a3
Faris Redza <faris.redza1999@gmail.com>
2024-02-07 18:22:09 +0000

    fixed udev dir

--- daisy-appimage.sh
+++ daisy-appimage.sh
@@ -6,7 +6,7 @@ mkdir -p PKG_SOURCE/DEBIAN/
 cp debian/control PKG_SOURCE/DEBIAN/
 mkdir -p PKG_SOURCE/usr/lib/udev/rules.d/
 cp AppDir/DAISY/install/nhands.rules PKG_SOURCE/usr/lib/udev/rules.d/
-dpkg-deb --root-owner-group --build PKG_SOURCE daisy-nhands-rules.deb
+dpkg-deb --root-owner-group --build PKG_SOURCE daisy-nhands-rules_0.1-1_all.deb
 
 # Download appimage-builder
 wget -O appimage-builder-x86_64.AppImage https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.1.0/appimage-builder-1.1.0-x86_64.AppImage
